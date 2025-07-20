5e0fc1a38b06746450df59acfad03a149dd69d2d
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-20 23:55:21 +0100

    Added new Folder

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather_input = input("What's the weather like today? (sunny/rainy/cold):")
+if "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+if "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else :
+    print("Sorry, I don't have recommendations for this weather.")
